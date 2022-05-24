a008b8a0faa96ff1835ff2d5d40296b8fe2225fd
Roman <roman.semenikhin99@gmail.com>
2022-05-24 13:13:26 +0300

    changed db connection port constant

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 DATABASE=os.getenv("DATABASE")
 HOST=os.getenv("HOST")
-PG_PORT="5432"
+PG_PORT=os.getenv("PG_PORT")
 API_TOKEN = os.getenv('API_TOKEN')
 USER = os.getenv('USER')
 PASSWORD = os.getenv('PASSWORD')
